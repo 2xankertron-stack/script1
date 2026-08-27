ab6dc93f09d92e5713c2b75610cf97837349a0ca
2xankertron-stack <2xankertron@gmail.com>
2026-08-27 02:25:45 -0700

    Remove clip age filter UI

--- clipforge-site/app.py
+++ clipforge-site/app.py
@@ -1,9 +1,7 @@
 import studio_app
 from preview_patch import apply_preview_patch
-from age_patch import apply_age_patch, register_age_routes
 from ai_editor_patch import apply_ai_editor_patch, register_ai_routes
 
-studio_app.HTML = apply_ai_editor_patch(apply_age_patch(apply_preview_patch(studio_app.HTML)))
-register_age_routes(studio_app.app)
+studio_app.HTML = apply_ai_editor_patch(apply_preview_patch(studio_app.HTML))
 register_ai_routes(studio_app.app)
 app = studio_app.app
