53b653bc1d1a09c5e210face301c0537ac830cfa
2xankertron-stack <2xankertron@gmail.com>
2026-08-27 03:03:03 -0700

    Remove AI Editor from ClipForge

--- clipforge-site/app.py
+++ clipforge-site/app.py
@@ -1,9 +1,5 @@
 import studio_app
-from ai_editor_patch import apply_ai_editor_patch, register_ai_routes
-from ai_render_fix import register_ai_render_fix
 from remove_short_editor_patch import remove_short_editor
 
-studio_app.HTML = remove_short_editor(apply_ai_editor_patch(studio_app.HTML))
-register_ai_routes(studio_app.app)
-register_ai_render_fix(studio_app.app)
+studio_app.HTML = remove_short_editor(studio_app.HTML)
 app = studio_app.app
