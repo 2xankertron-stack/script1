1daef5882e57db7fb1339971f94418063b7a19c1
2xankertron-stack <2xankertron@gmail.com>
2026-08-27 02:14:03 -0700

    Wire clip age filter into ClipForge

--- clipforge-site/app.py
+++ clipforge-site/app.py
@@ -1,5 +1,7 @@
 import studio_app
 from preview_patch import apply_preview_patch
+from age_patch import apply_age_patch, register_age_routes
 
-studio_app.HTML = apply_preview_patch(studio_app.HTML)
+studio_app.HTML = apply_age_patch(apply_preview_patch(studio_app.HTML))
+register_age_routes(studio_app.app)
 app = studio_app.app
